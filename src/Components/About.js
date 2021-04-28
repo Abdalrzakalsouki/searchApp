b7cf264e34e09d690c5882a74e1010b76c24ccd6
Abdulrazzak Alsssouki <abdulrzakalsssouki@gmail.com>
2021-04-28 12:56:47 -0700

    Update About page

--- src/Components/About.js
+++ src/Components/About.js
@@ -4,7 +4,9 @@ export default function About() {
   return (
     <div>
       <h2>Building Search Engine V1.0.0</h2>
-      <h4>Devloped for Tusgrame company by Abdulrazzak Alsssouki</h4>
+      <h4>
+        Devloped for Tungsram Innovative Solutions Kft by Abdulrazzak Alsssouki
+      </h4>
     </div>
   );
 }
