88912ab722bd50593e3718363838f03b9255b469
Abdulrazzak Alsssouki <abdulrzakalsssouki@gmail.com>
2021-05-05 17:57:03 -0700

    replacing cards

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import './App.css';
-import Home from './Components/Home';
-import About from './Components/About';
-import Nav from './Components/Nav';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import "./App.css";
+import Home from "./Components/Home";
+import About from "./Components/About";
+import Nav from "./Components/Nav";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-   
     <div className="App">
       <Router>
-       <Nav/>
+        <Nav />
         <Switch>
-           <Route path="/" exact component={Home} />
-           <Route path="/about" component={About} />
+          <Route path="/" exact component={Home} />
+          <Route path="/about" component={About} />
         </Switch>
       </Router>
     </div>
-    
   );
 }
 
--- src/Components/Result.js
+++ src/Components/Result.js
@@ -1,12 +1,28 @@
 import React from "react";
-import { Card, Heading, Text } from "evergreen-ui";
+import { Card, Container } from "react-bootstrap";
 import "../App.css";
 
 export default function Result(props) {
   return (
     <div>
       {props.result.map((building) => (
-        <Card
+        <Container>
+          <Card style={{ width: "18rem" }}>
+            <Card.Header>
+              {building.location} || {building.number}{" "}
+            </Card.Header>
+            <Card.Body>
+              <Card.Title>{building.name}</Card.Title>
+              <Card.Text>{building.description}</Card.Text>
+            </Card.Body>
+          </Card>
+        </Container>
+      ))}
+    </div>
+  );
+
+  /*
+  <Card
           id="resultcard"
           elevation={4}
           width={400}
@@ -25,8 +41,6 @@ export default function Result(props) {
           <Heading>{building.location}</Heading>
           <Heading>{building.number}</Heading>
           key={building.id}
-        </Card>
-      ))}
-    </div>
-  );
+        </Car
+  */
 }
